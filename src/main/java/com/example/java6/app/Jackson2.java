f01ee563d141eb25fbf056ada85930fef47b75d1
lequynhanhtth <68950884+lequynhanhtth@users.noreply.github.com>
2021-07-05 13:47:50 +0700

    1.47

--- src/main/java/com/example/java6/app/Jackson2.java
+++ src/main/java/com/example/java6/app/Jackson2.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 public class Jackson2 {
     public static void main(String[] args) throws Exception {
-        demo3();
+        demo4();
     }
 
     private static void demo2() throws Exception {
